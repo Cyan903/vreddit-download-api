4a6877cb846c2feca1cb318917be28f6a98ab075
CyanPiano <cyanp0515@gmail.com>
2022-01-18 18:07:24 -0500

    Cleanup + util

--- src/util.ts
+++ src/util.ts
@@ -5,9 +5,9 @@ import metascraper from "metascraper";
 import metascraperVideo from "metascraper-video";
 
 type Links = {
-    error: string,
     code: number
-} | string[];
+    res: string | object
+};
 
 const scraper = metascraper([metascraperVideo()]);
 const getXML = got.extend(require("got-xml")());
@@ -20,8 +20,38 @@ async function getVideoID(vid: string): Promise<string | number> {
     return video ? new URL(video).pathname.split("/")[1] : 404;
 }
 
+function filterVideo(mpd: Response, id: string): object {
+    const data = mpd.MPD.Period[0].AdaptationSet;
+    let videos, audio;
+
+    for (let vid in data) {
+        if (data[vid].$.contentType == "video") {
+            videos = data[vid].Representation.map((vid) => {
+                return {
+                    height: vid.$.height,
+                    width: vid.$.width,
+                    framerate: vid.$.frameRate,
+                    url: `https://v.redd.it/${id}/${vid.BaseURL[0]}`,
+                }
+            });
+        } else if (data[vid].$.contentType == "audio") {
+            audio = `https://v.redd.it/${id}/${data[vid].Representation[0].BaseURL[0]}`;
+        }
+    }
+
+    return { id, videos, audio };
+}
+
 export async function getLinks(url: string): Promise<Links> {
     const id = await getVideoID(url);
+
+    if (id == 404) {
+        return {
+            res: "Request is not a video!",
+            code: 404,
+        };
+    }
+
     const { statusCode, body }: {
         statusCode: number,
         body: unknown
@@ -29,21 +59,15 @@ export async function getLinks(url: string): Promise<Links> {
         `https://v.redd.it/${id}/DASHPlaylist.mpd`
     );
 
-    if (id == 404) {
-        return {
-            error: "Request is not a video!",
-            code: 404,
-        };
-    }
-
     if (statusCode != 200) {
         return {
-            error: "Dash playlist not found.",
+            res: "Dash playlist not found.",
             code: 400,
         };
     }
 
-    return (body as Response).MPD.Period[0].AdaptationSet[1].Representation.map(
-        (vid) => `https://v.redd.it/${id}/${vid.BaseURL[0]}`
-    );
+    return {
+        code: 200,
+        res: filterVideo(body as Response, String(id))
+    }
 }
